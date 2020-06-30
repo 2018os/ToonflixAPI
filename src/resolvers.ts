dcec473d7afdffada5dd63d8459972f3ffe2f1ee
seungyeop Kim <seungyeop2002@gmail.com>
2020-06-30 20:10:42 +0900

    Add basic queries with dummyData

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,63 @@
+const dummyWebtoons = [
+  {
+    id: 'webtoon-1',
+    title: 'webtoon1',
+    description: 'desc',
+    platform: 'NAVER',
+    isFinish: true,
+    isAdult: false,
+    isPay: false,
+    thumbnail: 'http:www.naver.com',
+    source: 'http:www.naver.com',
+    authors: [
+      {
+        id: 'author-1',
+        name: 'author1'
+      },
+      {
+        id: 'author-2',
+        name: 'author2'
+      }
+    ]
+  },
+  {
+    id: 'webtoon-2',
+    title: 'webtoon2',
+    description: 'desc2',
+    platform: 'DAUM',
+    isFinish: true,
+    isAdult: false,
+    isPay: false,
+    thumbnail: 'http:www.daum.com',
+    source: 'http:www.daum.com',
+    authors: [
+      {
+        id: 'author-1',
+        name: 'author1'
+      },
+      {
+        id: 'author-2',
+        name: 'author2'
+      }
+    ]
+  }
+];
+
+const dummyAuthors = [
+  {
+    id: 'author-1',
+    name: 'author1'
+  },
+  {
+    id: 'author-2',
+    name: 'author2'
+  }
+];
+
 const resolvers = {
   Query: {
-    hello: (): string => 'Hello'
+    webtoons: () => dummyWebtoons,
+    authors: () => dummyAuthors
   }
 };
 
