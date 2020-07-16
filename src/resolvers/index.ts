111b119674ab47f8d74800e711e0733b55b4c934
seungyeop Kim <seungyeop2002@gmail.com>
2020-07-16 16:44:54 +0900

    Remove comments

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,11 +1,9 @@
 import * as Query from './Query';
 import Type from './Type';
-// import * as Mutation from './Mutation';
 
 const resolvers = {
   Query,
   ...Type
-  // Mutation
 };
 
 export default resolvers;
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -27,39 +27,4 @@ async function webtoons(
   return allWebtoons;
 }
 
-// async function webtoon(_parent: any, args: any, context: Context, _info: any) {
-//   const result = await context.prisma.webtoon.findOne({
-//     where: {
-//       id: args.id
-//     }
-//   });
-//   return result;
-// }
-
-// async function authors(_parent: any, _args: any, context: Context, _info: any) {
-//   const allAuthors = await context.prisma.author.findMany();
-//   return allAuthors;
-// }
-
-// async function genres(_parent: any, _args: any, context: Context, _info: any) {
-//   const allGenres = await context.prisma.genre.findMany();
-//   return allGenres;
-// }
-
-// async function collections(
-//   _parent: any,
-//   _args: any,
-//   context: Context,
-//   _info: any
-// ) {
-//   const allCollections = await context.prisma.collection.findMany({
-//     include: {
-//       webtoons: true
-//     }
-//   });
-//   return allCollections;
-// }
-
-// export { webtoons, webtoon, authors, genres, collections };
-
 export { webtoons };
