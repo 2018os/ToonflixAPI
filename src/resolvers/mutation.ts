97decc48bfbe00176ecfe57a7be7f927c34738a5
seungyeop Kim <seungyeop2002@gmail.com>
2020-07-30 19:41:06 +0900

    Draft 'signup' & 'login' mutation

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,6 +1,14 @@
+import { ApolloError } from 'apollo-server-express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 import { Context } from '../utils/context';
-import { COLLECTION_ID_UNIT } from '../utils/unit';
-import { CollectionInputArgument } from './types';
+import { AUTH_TOKEN, COLLECTION_ID_UNIT, USER_ID_UNIT } from '../utils/statics';
+import {
+  CollectionInputArgument,
+  LoginArgument,
+  SignupArgument
+} from './types';
 
 type webtoonConnect = {
   id: string;
@@ -29,4 +37,39 @@ async function createCollection(
   return collection;
 }
 
-export { createCollection };
+async function signup(_parent: any, args: SignupArgument, context: Context) {
+  const { name, email, password } = args.input;
+  const existed = await context.prisma.user.findOne({ where: { email } });
+  if (existed) throw new ApolloError('Already Existing Email', 'INVALID_DATA');
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const userCounts = await context.prisma.user.count();
+  const id = Buffer.from(String(USER_ID_UNIT + userCounts)).toString('base64');
+  const user = await context.prisma.user.create({
+    data: {
+      id,
+      name,
+      email,
+      password: hashedPassword
+    }
+  });
+  const token = jwt.sign({ userId: user.id }, AUTH_TOKEN);
+  return {
+    token,
+    user
+  };
+}
+
+async function login(_parent: any, args: LoginArgument, context: Context) {
+  const { email, password } = args.input;
+  const user = await context.prisma.user.findOne({ where: { email } });
+  if (!user) throw new ApolloError('Not Found User', 'INVALID_DATA');
+  const valid = bcrypt.compareSync(password, user.password);
+  if (!valid) throw new ApolloError('Invalid password', 'INVALID_DATA');
+  const token = jwt.sign({ userId: user.id }, AUTH_TOKEN);
+  return {
+    token,
+    user
+  };
+}
+
+export { createCollection, signup, login };
