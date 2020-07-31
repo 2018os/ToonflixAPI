f1266e4f5e9ca165e0bd883a7bc218252f52d933
seungyeop Kim <seungyeop2002@gmail.com>
2020-07-31 16:11:03 +0900

    Add function to check valid user by jwt

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,6 +1,20 @@
+import { ApolloError } from 'apollo-server-express';
+import jwt from 'jsonwebtoken';
+
+import { AUTH_TOKEN } from './statics';
+import { Context } from './context';
+
 function arrayToObjectArrayConverter(array: any[], key: string): any[] {
   const result = array ? array.map((value) => ({ [key]: value })) : [];
   return result;
 }
 
-export { arrayToObjectArrayConverter };
+function getUserId(context: Context): object | string {
+  const Authorization = context.req.get('Authorization');
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '');
+    return jwt.verify(token, AUTH_TOKEN);
+  }
+  throw new ApolloError('No Authentication User', 'NOT_AUTHENTICATION');
+}
+export { arrayToObjectArrayConverter, getUserId };
