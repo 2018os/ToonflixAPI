d15ee39494d28b7c27245cb728cc8e4c5d519725
seungyeop Kim <seungyeop2002@gmail.com>
2020-09-02 17:22:04 +0900

    Draft interface FieldConnection

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -9,7 +9,11 @@ const Type = {
     },
     pageInfo: async (root: ConnectionTypeRootArgument) => {
       const { data, delegate } = root;
-      const findAllData = await delegate.findMany();
+      const findAllData = await delegate.findMany({
+        orderBy: {
+          title: 'asc'
+        }
+      });
       const startCursor = data[0].id;
       const endCursor = data.slice(-1)[0].id;
       const lastWebtoonCursor = findAllData.slice(-1)[0].id;
@@ -28,6 +32,35 @@ const Type = {
   Edge: {
     cursor: (parent: Node) => parent.id,
     node: (parent: Node) => parent
+  },
+  Webtoon: {
+    authorsConnection: (root: any, args: any) => {
+      const { authors } = root;
+      const { cursor, take } = args.page;
+      const cursorIndex = cursor
+        ? authors.findIndex((element: Node) => element.id === cursor) + 1
+        : 0;
+      return {
+        data: authors,
+        edges: authors.slice(cursorIndex, take),
+        args: args.page
+      };
+    }
+  },
+  FieldConnection: {
+    edges: (root: any) => root.edges,
+    pageInfo: (root: any) => {
+      const { data, edges, args } = root;
+      const { cursor: startCursor } = args;
+      const endCursor = edges.slice(-1)[0] ? edges.slice(-1)[0].id : '';
+      return {
+        startCursor,
+        endCursor,
+        hasNextPage: endCursor !== data.slice(-1)[0].id,
+        hasPreviousPage: startCursor !== data[0].id
+      };
+    },
+    totalCounts: (root: any) => root.data.length
   },
   SearchResultConnection: {
     webtoonResult: (parent: any) => parent.webtoonResult,
