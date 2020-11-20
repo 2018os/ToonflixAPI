205f24ef8e22f3066549cd8c984896f15b1ecd6c
seungyeop Kim <seungyeop2002@gmail.com>
2020-11-20 14:36:30 +0900

    Change custom matcher name 'toIncludeObjects'

--- src/__test__/jest.d.ts
+++ src/__test__/jest.d.ts
@@ -2,8 +2,7 @@ export {};
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toBeOdd(x: number): R;
-      toObjectContain(properties: any[]): R; // TODO: Rename: Object containing at least one in properties
+      toIncludeObjects(properties: any[]): R;
     }
   }
 }
