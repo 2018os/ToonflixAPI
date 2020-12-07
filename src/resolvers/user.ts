e14fab234397825b15e5ed1fb97d009e26fd0cf6
seungyeop Kim <seungyeop2002@gmail.com>
2020-12-07 15:38:08 +0900

    Implement 'likedCollection' resolver on user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -33,5 +33,31 @@ export default {
       });
       return nodes;
     }
+  }),
+  likedCollections: connection({
+    cursorFromNode: (node: Node) => decodeCursor(node.id),
+    nodes: async (parent: User, args, context: Context) => {
+      const cursor = args.after || args.before;
+      const encodedCursor = cursor && encodeCursor(cursor);
+      const nodes = await context.prisma.collection.findMany({
+        where: {
+          likers: {
+            some: {
+              id: parent.id
+            }
+          }
+        },
+        skip: cursor ? 1 : undefined,
+        cursor: cursor
+          ? {
+              id: encodedCursor || undefined
+            }
+          : undefined,
+        orderBy: {
+          id: args.before ? 'desc' : 'asc'
+        }
+      });
+      return nodes;
+    }
   })
 };
