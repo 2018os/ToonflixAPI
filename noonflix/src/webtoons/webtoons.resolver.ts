e1bb972eb1701c0227c3fe69e805c99742c79969
seungyeop Kim <seungyeop2002@gmail.com>
2021-03-01 15:47:38 +0900

    Add query 'webtoon', 'randomWebtoons'

--- noonflix/src/webtoons/webtoons.resolver.ts
+++ noonflix/src/webtoons/webtoons.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args, ID, Int } from '@nestjs/graphql';
 
 import { WebtoonsService } from './webtoons.service';
 
@@ -8,8 +8,20 @@ import { Webtoon } from './entities/webtoon.entity';
 export class WebtoonsResolver {
   constructor(private readonly webtoonsService: WebtoonsService) {}
 
-  @Query(() => [Webtoon], { name: 'webtoons', nullable: true })
-  findAll(): Promise<Webtoon[] | null> {
+  @Query(() => [Webtoon], { name: 'webtoons' })
+  findAll(): Promise<Webtoon[]> {
     return this.webtoonsService.findAll();
+  }
+
+  @Query(() => Webtoon, { name: 'webtoon' })
+  findOne(@Args('id', { type: () => ID }) id: string): Promise<Webtoon> {
+    return this.webtoonsService.findOne(id);
+  }
+
+  @Query(() => [Webtoon], { name: 'randomWebtoons' })
+  findRandomWebtoons(
+    @Args('take', { type: () => Int }) take: number,
+  ): Promise<Webtoon[]> {
+    return this.webtoonsService.findRandomWebtoons(take);
   }
 }
--- noonflix/src/webtoons/webtoons.service.ts
+++ noonflix/src/webtoons/webtoons.service.ts
@@ -1,12 +1,36 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { PrismaService } from 'src/common/prisma/prisma.service';
+import { PrismaService } from '../common/prisma/prisma.service';
 import { Webtoon } from './entities/webtoon.entity';
 
 @Injectable()
 export class WebtoonsService {
   constructor(@Inject(PrismaService) private prismaService: PrismaService) {}
 
-  findAll(): Promise<Webtoon[] | null> {
+  findAll(): Promise<Webtoon[]> {
     return this.prismaService.webtoon.findMany();
+  }
+
+  findOne(id: string): Promise<Webtoon> {
+    return this.prismaService.webtoon.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  getRandom(max: number, count: number): number[] {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+      const random = Math.floor(Math.random() * max);
+      arr.includes(random) ? (i -= 1) : arr.push(random);
+    }
+    return arr;
+  }
+
+  async findRandomWebtoons(take: number): Promise<Webtoon[]> {
+    const allWebtoon = await this.findAll();
+    const randomIndexes = this.getRandom(allWebtoon.length, take);
+    const webtoons = randomIndexes.map((index) => allWebtoon[index]);
+    return webtoons;
   }
 }
